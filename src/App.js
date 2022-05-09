8642fb46360b18a52b4428d0154925614a5969ed
Sergey Vodyanov <VodyanovSV@gmail.com>
2022-05-09 12:09:15 +0300

    create project, routing, empty main component.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Main from "./pages/main/Main";
+import './App.css';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route exact path={'/'} element={<Main/>}/>
+                    <Route path={'*'} element={<Navigate to={'/'}/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Main.module.scss'
+
+const Main = () => {
+    return (
+        <div className={styles.container}>
+            main comp
+        </div>
+    );
+};
+
+export default Main;
